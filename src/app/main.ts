84b83c8dc01946d49fa3ccfbf3630d521b294731
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2022-09-23 07:06:37 -0300

    Listagem e Cadastro de Produto e Serviço

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,7 +1,11 @@
 import Entrada from "../io/entrada";
 import Empresa from "../modelo/empresa"
 import CadastroCliente from "../negocio/cadastroCliente";
+import CadastroProduto from "../negocio/cadastroProduto";
+import CadastroServico from "../negocio/cadastroServico";
 import ListagemClientes from "../negocio/listagemClientes";
+import ListagemProdutos from "../negocio/listagemProduto";
+import ListagemServicos from "../negocio/listagemServico";
 
 console.log(`Bem-vindo ao cadastro de clientes do Grupo World Beauty`)
 let empresa = new Empresa()
@@ -11,6 +15,10 @@ while (execucao) {
     console.log(`Opções:`);
     console.log(`1 - Cadastrar cliente`);
     console.log(`2 - Listar todos os clientes`);
+    console.log('3 - Cadastro de produtos');
+    console.log('4 - Listagem de produtos');
+    console.log('5 - Cadastro de serviços');
+    console.log('6 - Listagem de serviços');
     console.log(`0 - Sair`);
 
     let entrada = new Entrada()
@@ -18,12 +26,28 @@ while (execucao) {
 
     switch (opcao) {
         case 1:
-            let cadastro = new CadastroCliente(empresa.getClientes)
-            cadastro.cadastrar()
+            let cadastroCliente = new CadastroCliente(empresa.getClientes)
+            cadastroCliente.cadastrar()
             break;
         case 2:
-            let listagem = new ListagemClientes(empresa.getClientes)
-            listagem.listar()
+            let listagemClientes = new ListagemClientes(empresa.getClientes)
+            listagemClientes.listar()
+            break;
+        case 3:
+            let cadastroProduto = new CadastroProduto(empresa.getProdutos);
+            cadastroProduto.cadastrar()
+            break;
+        case 4:
+            let listagemProdutos = new ListagemProdutos(empresa.getProdutos);
+            listagemProdutos.listar()
+            break;
+        case 5:
+            let cadastroServico = new CadastroServico(empresa.getProdutos);
+            cadastroServico.cadastrar()
+            break;
+        case 6:
+            let listagemServicos = new ListagemServicos(empresa.getProdutos);
+            listagemServicos.listar()
             break;
         case 0:
             execucao = false
